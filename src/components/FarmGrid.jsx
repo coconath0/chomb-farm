8ff3089c37de6c3ba78c678cfc6f36b3378dbd94
Coconath <naingol10@gmail.com>
2026-03-20 19:20:40 -0500

    feat: enhance FarmGrid to pass dispatch and chombRoster to PlotTile for improved functionality

--- src/components/FarmGrid.jsx
+++ src/components/FarmGrid.jsx
@@ -3,12 +3,18 @@ import PlotTile from "./PlotTile";
 import styles from "./FarmGrid.module.css";
 
 export default function FarmGrid() {
-    const { state } = useFarm();
+    const { state, dispatch } = useFarm();
+    const { chombRoster } = state;
 
     return (
         <div className={styles.grid}>
             {state.plots.map((plot) => (
-                <PlotTile key={plot.id} plot={plot} />
+                <PlotTile
+                    key={plot.id}
+                    plot={plot}
+                    chombRoster={chombRoster}
+                    dispatch={dispatch}
+                />
             ))}
         </div>
     );
